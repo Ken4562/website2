648b63535ae6133d13d25a1f7084f8e55d1e2ddc
Ken <luzekai878@gmail.com>
2024-05-26 12:27:04 +0800

    e

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, request, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', template_folder='templates')
 
-@app.route("/")
-def home():
-    return render_template("index.html")
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/submit-form', methods=['POST'])
+def submit_form():
+    name = request.form['name']
+    email = request.form['email']
+    subject = request.form['subject']
+    message = request.form['message']
+    # 處理表單數據，例如保存到數據庫或發送郵件
+    return '表單提交成功！'
+
+if __name__ == '__main__':
+    app.run(debug=True)
